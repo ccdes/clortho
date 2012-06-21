e563b93d11e0ce82ad21d39f3699f61e7ef870a7
cc <cc@nycheads.com>
2012-06-21 12:49:32 -0400

    a bit faster now.
    looks more like an external plugin.

--- plugin.c
+++ plugin.c
@@ -5,59 +5,52 @@
 unsigned long long startr=576470088000001;
 unsigned long long endr=576472224000001;
 
-char word[9];
+char word[8];
 char charset[]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*@#!$%^";
 int base;
+int leng;
 
 void init(void)
 {
 	base = strlen(charset);
+	leng = 1;
 }	
-
-void i2p(unsigned long long num, char *word)
-{
-	int apos, bpos;
-	int range[30];
-	apos=0;
-	if (num == 0) {
-		word[0]=charset[0];
-		word[1]=0;
-		return;
-	}
-	while (num) {
-		range[apos]= num % base;
-		num /= base;
-		apos++;
-	}
-	bpos=0;
-	while (apos) {
-		word[bpos]=charset[range[apos-1]];
-		bpos++;
-		apos--;
-	}
-	word[bpos]=0;
-	return;
-}
-
 
 void generate()
 {
-	unsigned long long countr;
-	countr=startr;
-	while (countr <= endr) {
-		i2p(countr, word);
-		printf("%s\n", word);
-		countr++;
+	int apos;
+	unsigned long long count;
+	if (startr == 0) {
+		word[0]=charset[0];
+		word[1]=0;
+		startr++;
+		return;
 	}
+	expand:
+		count=startr;
+		apos=leng;
+		word[--apos]=0;
+		while (count) {
+			if (apos < 0) {
+				leng++;
+				goto expand;
+			}
+			word[apos]=charset[ count % base];
+			count /= base;
+			apos--;
+		}
+	startr++;
 		
 }
-
 
 int main(int argc, char *argv[]) {
 
 	startr=strtoull(argv[1],NULL,0);
 	endr=strtoull(argv[2],NULL,0);
 	init();
-	generate();
+	while (startr <= endr) {
+		generate();
+		printf("%s\n", word);
+	}
 	return 0;
 }
